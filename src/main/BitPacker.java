ff72a2a73b0851a4f029dfd7e56419c987d1e6bc
akramdaboussi <akram.daboussi@etu.unice.fr>
2025-10-28 13:05:12 +0100

    Creation d'une interface BitPacker

---
+++ src/main/BitPacker.java
@@ -0,0 +1,28 @@
+package main;
+
+/**
+ * Interface pour les compresseurs de type Bit-Packing.
+ * Définit les méthodes communes requises par le projet.
+ */
+public interface BitPacker {
+
+    /**
+     * Compresse un tableau d'entiers.
+     * @param tab Le tableau d'entiers à compresser.
+     * @return Le tableau d'entiers compressé.
+     */
+    int[] compress(int[] tab);
+
+    /**
+     * Décompresse le tableau entier.
+     * @return Le tableau original décompressé.
+     */
+    int[] decompress();
+
+    /**
+     * Retourne la i-ème valeur du tableau original à partir des données compressées.
+     * @param i L'index de la valeur à récupérer.
+     * @return La valeur originale.
+     */
+    int get(int i);
+}
