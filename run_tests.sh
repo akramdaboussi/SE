5f763825bb290fd8e2fb5b78e2bd863c007cb1af
akramdaboussi <akram.daboussi@etu.unice.fr>
2025-10-29 11:55:24 +0100

    maj du script pour demander de choisir le type de compresseur

--- run_tests.sh
+++ run_tests.sh
@@ -6,27 +6,52 @@ set -e
 # Compile tous les .java de src/main et place les .class à la racine
 find src/main -name "*.java" -exec javac -d . {} +
 
+# --- 2. Sélection du type de compression ---
+echo -e "\n--- Choix du type de compression ---"
+PS3="Quel type de compresseur utiliser ? : "
+options_compression=("no_overlap" "overlap" "Quitter")
+
+# On stocke le choix dans la variable $comp_type
+select comp_type in "${options_compression[@]}"; do
+    case "$comp_type" in
+        "no_overlap" | "overlap")
+            echo "Mode sélectionné : $comp_type"
+            break
+            ;;
+        "Quitter")
+            echo "Quitter le script."
+            exit 0
+            ;;
+        *)
+            echo "Choix invalide. Veuillez réessayer."
+            ;;
+    esac
+done
+
+# Si $comp_type est vide (l'utilisateur a fait Ctrl+D), on quitte
+if [[ -z "$comp_type" ]]; then
+    exit 1
+fi
+
 # Définit le dossier des tests
 TEST_DIR="src/tests"
 JAVA_MAIN_CLASS="main.Main"
 
-# --- Exécution d'un test ---
+# --- 3. Exécution d'un test ---
 echo -e "\n--- Sélection du fichier de test ---"
+PS3="Entrez le numéro du test (ou 'q' pour quitter) : "
 
-# Configure le prompt du menu
-PS3="Entrez le numéro du test : "
-
-# On stocke les chemins complets dans un tableau
 files_with_path=("$TEST_DIR"/*.txt)
-
-# On crée un tableau d'options avec juste les noms de fichiers
 options_display=()
 for f in "${files_with_path[@]}"; do
     options_display+=("$(basename "$f")")
 done
 
-# On lance 'select' sur le tableau des noms (options_display)
 select opt_display in "${options_display[@]}"; do
+    if [[ "$REPLY" == "q" ]]; then
+        echo "Quitter le script."
+        break
+    fi
 
     case "$opt_display" in
         "Quitter")
@@ -34,21 +59,19 @@ select opt_display in "${options_display[@]}"; do
             break
             ;;
         *)
-            # On vérifie si l'option est valide
             if [[ -n "$opt_display" ]]; then
-            
-                # On récupère le chemin complet
                 full_path="${files_with_path[$((REPLY - 1))]}"
                 
-                java "$JAVA_MAIN_CLASS" "$full_path"
-                break 
+                # On passe le $full_path ET le $comp_type au Main
+                java "$JAVA_MAIN_CLASS" "$full_path" "$comp_type"
                 
+                echo "-----------------------------------------------------"
+                break 
             else
                 echo "Choix invalide. Veuillez réessayer."
             fi
             ;;
     esac
-    # Ré-affiche le prompt (seulement si choix invalide)
     echo "$PS3"
 done
 
