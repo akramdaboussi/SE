457ce61923fa8e9ad34d6a9d9de7cf1186eded94
akramdaboussi <akram.daboussi@etu.unice.fr>
2025-10-30 13:11:45 +0100

    ajout de la gestion du compresseur avec overflow dans le main

--- src/main/app/Main.java
+++ src/main/app/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Usage: java main.Main <chemin_vers_le_fichier> <type_compression>");
-            System.out.println("Types valides : 'overlap' ou 'no_overlap'");
+            System.out.println("Types valides : 'overlap', 'no_overlap', 'with_overflow'");
             return;
         }
         
@@ -24,9 +24,11 @@ public class Main {
             compressionType = CompressionType.WITH_OVERLAP;
         } else if ("no_overlap".equalsIgnoreCase(typeString)) {
             compressionType = CompressionType.NO_OVERLAP;
+        } else if ("with_overflow".equalsIgnoreCase(typeString)) {
+            compressionType = CompressionType.WITH_OVERFLOW;
         } else {
             System.out.println("Erreur : Type de compression '" + typeString + "' non valide.");
-            System.out.println("Types valides : 'overlap' ou 'no_overlap'");
+            System.out.println("Types valides : 'overlap', 'no_overlap', 'with_overflow'");
             return;
         }
         BenchmarkRunner runner = new BenchmarkRunner(filePath, compressionType);
