4691187b3f1139656169618ca2a6f0913b755c09
akramdaboussi <akram.daboussi@etu.unice.fr>
2025-10-30 12:40:41 +0100

    ajout du 3eme type de compression (avec overflow)

--- src/main/compression/CompressionType.java
+++ src/main/compression/CompressionType.java
@@ -2,5 +2,6 @@ package main.compression;
 
 public enum CompressionType {
     NO_OVERLAP,     // Sans chevauchement
-    WITH_OVERLAP    // Avec chevauchement
+    WITH_OVERLAP,    // Avec chevauchement
+    WITH_OVERFLOW    // Avec gestion d'overflow
 }
